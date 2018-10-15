24753a23e6f3a8822c37d110656379f763ef02c3
Shawn Rutledge <s@ecloud.org>
2018-10-15 11:20:28 +0200

    Move the timer to QML; add a toolbar with "update" switch
    This allows us to turn updating off, e.g. to be able to
    examine a row for a longer period.

--- qtquick/processmodel.h
+++ qtquick/processmodel.h
@@ -19,16 +19,12 @@ public:
     QVariant headerData(int section, Qt::Orientation orientation,
                         int role = Qt::DisplayRole) const override;
     Q_INVOKABLE int columnWidth(int c, const QFont *font = nullptr);
-
-protected:
-    void timerEvent(QTimerEvent *event) override;
-    void update();
+    Q_INVOKABLE void update();
 
 private:
     Proc m_proc; // the proctologist
     QVector<int> m_pids;
     QVector<int> m_columnWidths = QVector<int>(F_CMDLINE + 1);
-    int m_timerId;
 };
 
 #endif // PROCESSMODEL_H
